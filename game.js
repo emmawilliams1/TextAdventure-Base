b17d742dd583aeac0cdac45fb827d7a35c5e2eab
emmawilliams1 <emma.williams1@marist.edu>
2014-11-17 21:40:24 -0500

    Update game.js
    Final

--- game.js
+++ game.js
@@ -13,30 +13,46 @@ var player = {
 			this.items.splice(pos, 1)
 		}
 	}
+	var report = function (){
+		for (i=0; i < player.item.length; i++){
+			var inventoryHTML = querySelector('ul');
+			return inventoryHTML += this.items[i];
+		}
+	}
 }
 
 function interpret ( input ) {
-	var trimmedInput = input.toLowerCase.trim;
+	var trimmedInput = input.toLowerCase().trim();
 	var splitInput = trimmed.Input.split(' ');
-	var action = splitInput[0];
-	splitInput.remove(0);
+	//var action = splitInput[0];
+	//splitInput.remove(0);
 	var inventory = '';
 	for(i = 1; i < splitInput.length; i++){
-		inventory = inventory + splitInput[i] + ' ';
-	}
+		inventory.push(trimmedInput[i] + ' '); 
+	};
 	return inventory;
-}
+};
 
 function execute ( inventory ){
-	var action = inventory[0];
-	
-}
-
-function report () {
-	for (i=0; i < player.items.length; i++){
-		var inventoryHTML = querySelector('ul');
-		inventoryHTML += player.items[1];
+	var action = inventory.splice(0,0);
+	var object = inventory;
+	var action = function(object){
 	}
+};
+
+var report = function(inventory){
+	var list = document.getElementById('inventoryList');
+	for (var i = 0; i < inventory.length; i++) {
+		var li = document.createElement('li')
+		li.appendChild(document.createTextNode(inventory[i]));
+		list.appendChild(li);
+	};
+};
+
+var gameStep = function (){
+	var cmd = interpret(input);
+	var result = execute(cmd);
+	report(result);
 }
 
 var gameStart = function(){
@@ -46,6 +62,6 @@ var gameStart = function(){
 			gameStep(this.value);
 		}
 	});
-}
+};
 
 window.onload = gameStart;
