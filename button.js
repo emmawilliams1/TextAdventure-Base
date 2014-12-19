c2f01da34818cca86d12cce5a3839ca0e1c470cf
emmawilliams1 <emma.williams1@marist.edu>
2014-12-19 09:10:52 -0500

    Update button.js

--- button.js
+++ button.js
@@ -1,6 +1,15 @@
-function endGame() {
-	// if (){
-		// button.onclick === 1;
-			document.getElementById('body').innerHTML = 'Thank\'s for playing, hope you enjoyed my game! Bye!';
-	// }
+function startGame(){
+	if ($('#character').val() == '') {
+		$('p').empty().html('Please enter a character name');
+	}
+	else{
+		console.log($('input:radio[name=advantage]:checked').val());
+		name = $('#character').val() ;
+		advantage = $('input:radio[name=advantage]:checked').val();
+		window.location.href = "game.html?name=" + name + "&advantage=" + advantage;
+	}
+}
+
+function quitGame(){
+	window.location.href = 'end.html';
 }
